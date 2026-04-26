5c011f980d2b3f9018f9d22d441d4de225887727
paperbox <7126454+lego37yoon@users.noreply.github.com>
2026-04-26 12:10:47 +0900

    cont: apply current patrons

--- src/data/patrons.ts
+++ src/data/patrons.ts
@@ -1,45 +1,21 @@
 export const patrons = [
     {
-        name: "윤성수",
-        org: "시토리"
+        name: "임채형",
+        org: "(주)아이메코"
     },
     {
         name: "한영빈",
         org: "우분투한국커뮤니티"
     },
     {
-        name: "유현아",
-        org: "Foss for All"
+        name: "문성준",
+        org: "-"
     },
-    {
-        name: "김석현",
-        org: "(주)볼타코퍼레이션"
-    },
-    {
-        name: "정재훈",
-        org: "MONOLITH INC."
-    },
-    {
-        name: "강성진",
-        org: ""
-    }
 ]
 
 export const ukc = [
     {
         name: "한영빈",
         org: "우분투한국커뮤니티"
-    },
-    {
-        name: "차의서",
-        org: "우분투한국커뮤니티"
-    },
-    {
-        name: "방기연",
-        org: "C++ Korea"
-    },
-    {
-        name: "김세영",
-        org: "Canonical Ltd."
     },
 ]
