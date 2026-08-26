4b614c8e5a0815031398068375d4d496c40669ee
paperbox <7126454+lego37yoon@users.noreply.github.com>
2026-08-27 01:22:08 +0900

    cont: 키사이닝 파티 안내 추가

--- src/config.ts
+++ src/config.ts
@@ -75,10 +75,10 @@ export const WebsiteConfig = {
                         label: m["nav_programs_speakers"](),
                         link: "https://events.canonical.com/event/153/contributions/speakers"
                     },
-            //         {
-            //             label: m["nav_programs_social"](),
-            //             link: "https://github.com/ubuntu-kr/ksp-toolkits/blob/master/ksp/ksp-20250809/readme.md"
-            //         },
+                    {
+                        label: m["nav_programs_social"](),
+                        link: "https://github.com/ubuntu-kr/ksp-toolkits/blob/master/ksp/ksp-20260829/readme.md"
+                    },
                     {
                         label: m["nav_programs_lunch"](),
                         link: `/${locale}/programs/lunch`
